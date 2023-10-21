c2178e61a1167f6daecdc98ae08e262678da20f7
Dickson Wong <dicksonwong@gmail.com>
2023-10-21 13:02:59 -0700

    Update job gauges visibility after job change

--- FaderPlugin/Plugin.cs
+++ FaderPlugin/Plugin.cs
@@ -31,6 +31,11 @@ public class Plugin : IDalamudPlugin {
     private readonly Timer idleTimer = new();
     private bool hasIdled;
 
+    // Job Change
+    private uint currentJobId;
+    private readonly Timer jobChangeReadyTimer = new();
+    private bool isJobChangeReady;
+
     // Chat State
     private readonly Timer chatActivityTimer = new();
     private bool hasChatActivity;
@@ -80,6 +85,14 @@ public class Plugin : IDalamudPlugin {
             hasIdled = true;
         };
         idleTimer.Start();
+
+        // Fired once after job change
+        jobChangeReadyTimer.AutoReset = false;
+        jobChangeReadyTimer.Interval = 100;
+        jobChangeReadyTimer.Elapsed += (_, _) => {
+            isJobChangeReady = true;
+        };
+        jobChangeReadyTimer.Start();
 
         chatActivityTimer.Elapsed += (_, _) => {
             hasChatActivity = false;
@@ -218,12 +231,15 @@ public class Plugin : IDalamudPlugin {
         var boundByDuty = Condition[ConditionFlag.BoundByDuty] || Condition[ConditionFlag.BoundByDuty56] || Condition[ConditionFlag.BoundByDuty95];
         UpdateStateMap(State.Duty, !inIslandSanctuary && boundByDuty);
 
+        stateChanged |= isJobChangeReady;
+
         // Only update display state if a state has changed.
         if(stateChanged || hasIdled || Addon.HasAddonStateChanged("HudLayout")) {
             UpdateAddonVisibility();
 
-            // Always set Idled to false to prevent looping
+            // Set single-fired timers to false to prevent looping
             hasIdled = false;
+            isJobChangeReady = false;
 
             // Only start idle timer if there was a state change
             if(stateChanged && config.DefaultDelayEnabled) {
@@ -232,6 +248,12 @@ public class Plugin : IDalamudPlugin {
                 idleTimer.Interval = config.DefaultDelay;
                 idleTimer.Start();
             }
+        }
+
+        // During job change, job gauges require a delay before it can be hidden
+        if (CheckJobChanged()) {
+            jobChangeReadyTimer.Stop();
+            jobChangeReadyTimer.Start();
         }
     }
 
@@ -270,7 +292,7 @@ public class Plugin : IDalamudPlugin {
                 List<ConfigEntry> elementConfig = config.GetElementConfig(element);
 
                 if (elementConfig.FirstOrDefault(entry => stateMap[entry.state]) is { } configEntry &&
-                    (configEntry.state != State.Default || !config.DefaultDelayEnabled || hasIdled))
+                    (configEntry.state != State.Default || !config.DefaultDelayEnabled || hasIdled || isJobChangeReady))
                 {
                     setting = configEntry.setting;
                 }
@@ -284,6 +306,13 @@ public class Plugin : IDalamudPlugin {
                 Addon.SetAddonVisibility(addonName, setting == Setting.Show);
             }
         }
+    }
+
+    private bool CheckJobChanged() {
+        uint newJobId = ClientState.LocalPlayer?.ClassJob.RowId ?? 0;
+        bool changed = newJobId != currentJobId;
+        currentJobId = newJobId;
+        return changed;
     }
 
     /// <summary>
